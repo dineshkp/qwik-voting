83121fd3cb61c9ceef5e54e6296e10edea6b2c27
Dinesh Kumar P <64963503+dineshkp@users.noreply.github.com>
2023-11-17 03:49:11 +0530

    add: daisyui

--- src/root.tsx
+++ src/root.tsx
@@ -24,7 +24,7 @@ export default component$(() => {
         <RouterHead />
         <ServiceWorkerRegister />
       </head>
-      <body lang="en">
+      <body lang="en" data-theme='dracula'>
         <RouterOutlet />
       </body>
     </QwikCityProvider>
