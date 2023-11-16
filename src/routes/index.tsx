0e1e2fb3255606f54635c1eeb58b4ee21782830b
Dinesh Kumar P <64963503+dineshkp@users.noreply.github.com>
2023-11-17 03:58:51 +0530

    add: side-nav

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import type {DocumentHead} from "@builder.io/qwik-city";
 import {Link, routeLoader$} from "@builder.io/qwik-city";
 import prismaClient from "~/lib/prismaClient";
 
-export const useCategories = routeLoader$(async() => {
+export const useCategories = routeLoader$(async () => {
   return await prismaClient.category.findMany();
 })
 
@@ -11,27 +11,27 @@ export default component$(() => {
   const categories = useCategories();
 
   return (
-    <>
-      <div class="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {categories.value?.map((category) => (
-            <Link href={`/categories/${category.id}`} key={category.id}>
-              <div class="p-2">
-                <div class="card w-full glass">
-                  <figure>
-                    <img src={`/${category.image}`} alt={category.name} />
-                  </figure>
-                  <div class="card-body">
-                    <h2 class="card-title">{category.name}</h2>
-                    <div class="card-actions justify-end">
-                      <button class="btn btn-primary">Vote now!</button>
+      <>
+        <div class="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {categories.value?.map((category) => (
+              <Link href={`/categories/${category.id}`} key={category.id}>
+                <div class="p-2">
+                  <div class="card w-full glass">
+                    <figure>
+                      <img src={`/${category.image}`} alt={category.name}/>
+                    </figure>
+                    <div class="card-body">
+                      <h2 class="card-title">{category.name}</h2>
+                      <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Vote now!</button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-        ))}
-      </div>
-    </>
+              </Link>
+          ))}
+        </div>
+      </>
   );
 });
 
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,11 +1,12 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
-import type { RequestHandler } from "@builder.io/qwik-city";
+import {component$, Slot, useStyles$} from "@builder.io/qwik";
+import {Link, routeLoader$, useLocation} from "@builder.io/qwik-city";
+import type {RequestHandler} from "@builder.io/qwik-city";
 
 import styles from "./styles.css?inline";
 import Header from "~/components/header";
+import prismaClient from "~/lib/prismaClient";
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
+export const onGet: RequestHandler = async ({cacheControl}) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.builder.io/docs/caching/
   cacheControl({
@@ -16,20 +17,50 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
+export const useCategories = routeLoader$(async () => {
+  return await prismaClient.category.findMany();
+})
+
+const navItem = "p-2";
+const navItemActive = `${navItem} bg-gray-300 text-black rounded-md font-bold`;
 
 export default component$(() => {
+  const categories = useCategories();
+  const location = useLocation();
+
   useStyles$(styles);
+
   return (
-    <>
-      <Header />
-      <main>
-        <Slot />
-      </main>
-    </>
+      <>
+        <Header/>
+        <main class="p-2">
+          <div class="md:grid md:grid-cols-[20%_80%] gap-3">
+            <div class="flex flex-col">
+              <Link
+                  href="/"
+                  class={location.url.pathname === "/" ? navItemActive : navItem}
+              >
+                <div>Home</div>
+              </Link>
+              {categories.value?.map((category) => (
+                  <Link
+                      class={
+                        location.url.pathname === `/categories/${category.id}/`
+                            ? navItemActive
+                            : navItem
+                      }
+                      href={`/categories/${category.id}`}
+                      key={category.id}
+                  >
+                    <div>{category.name}</div>
+                  </Link>
+              ))}
+            </div>
+            <div>
+              <Slot/>
+            </div>
+          </div>
+        </main>
+      </>
   );
 });
